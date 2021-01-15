b5c0e16140be39e8fa9c68174f484b2b22ec795d
Mitchell Henke <mitchell.henke@gsa.gov>
2021-01-15 12:03:11 -0600

    fix lines

--- lib/identity_pki_web/controllers/page_controller.ex
+++ lib/identity_pki_web/controllers/page_controller.ex
@@ -11,7 +11,8 @@ defmodule IdentityPkiWeb.PageController do
   end
 
   defp referrer(conn, params) do
-    header_referrer = Plug.Conn.get_req_header(conn, "Referer")                      |> List.first()
+    header_referrer = Plug.Conn.get_req_header(conn, "Referer")
+                      |> List.first()
 
     param_referrer = Map.get(params, "redirect_uri")
     referrer = header_referrer || param_referrer
